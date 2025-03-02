730efb9182252009c3f2fb8089541466e45fe1b4
jwojci <jakubwojtek21@gmail.com>
2025-03-02 18:15:09 +0100

    added 60 fps lock

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        fps = clock.tick(60)
+        dt = fps/1000
 if __name__ == "__main__":
     main()
