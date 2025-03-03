67bbc37a7b7a91ae3483c27280ed34c3a03bb266
jwojci <jakubwojtek21@gmail.com>
2025-03-03 22:36:33 +0100

    added Player class and added displaying of the player object in the middle of the screen when starting game

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 
 from constants import * 
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
@@ -15,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         fps = clock.tick(60)
         dt = fps/1000
